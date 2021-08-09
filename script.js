76b417119ff7660323ccb28135c8af57e8ec868f
Edward <edwardi.reyes21@gmail.com>
2021-08-08 21:28:10 -0700

    Hovering over cells will now color them black

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 // Adds colors for color choice
-
 let color_container = document.querySelector(".color-container");
-
 let color_choices_names = ["Black", "Red", "Blue", "Purple", "Green"];
+let current_color = "black"; // Current color, default: black
 
 for (let i = 0; i < 5; i++) {
     let color_element = document.createElement("h1");
@@ -13,28 +12,16 @@ for (let i = 0; i < 5; i++) {
     color_container.appendChild(color_element);
 }
 
-// Creates 16x16 grid for the game
-
+// Creates 256 cells (16x16 grid)
 let grid = document.querySelector(".grid");
-
-// Creates 16 columns for the grid
-/*for (let i = 0; i < 16; i++) {
-    let grid_row = document.createElement("div");
-    grid_row.classList.add(".grid-col");
-
-    // For each column, creates 16 cells
-    for (let j = 0; j < 16; j++) {
-        let grid_cell = document.createElement("div");
-        grid_cell.classList.add(".grid-cell");
-        grid_cell.innerText = i;
-        grid_row.appendChild(grid_cell);
-    }
-
-    grid.appendChild(grid_row);
-}*/
 
 for (let i = 0; i < 16 * 16; i++) {
     let grid_cell = document.createElement("div");
     grid_cell.classList.add("grid-cell");
+    grid_cell.addEventListener("mouseover", (Event) => {
+        console.log(Event.target);
+        Event.target.style.background = current_color;
+    });
     grid.appendChild(grid_cell);
 }
+
