775068537ddeb8c47a34b9d3b53f38ebb64a6061
Edward <edwardi.reyes21@gmail.com>
2021-08-08 21:46:04 -0700

    Added option to choose more colors

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 // Adds colors for color choice
 let color_container = document.querySelector(".color-container");
-let color_choices_names = ["Black", "Red", "Blue", "Purple", "Green"];
+let color_choices_names = ["Black", "White", "Blue", "Purple", "Green"];
 let current_color = "black"; // Current color, default: black
 
+// Creates headers representing each possible color
 for (let i = 0; i < 5; i++) {
     let color_element = document.createElement("h1");
 
-    color_element.textContent = color_choices_names[i];
+    color_element.style.cursor = "pointer";
     color_element.style.color = color_choices_names[i];
+    color_element.style.stroke = "black";
+    color_element.textContent = color_choices_names[i];
+    
+    color_element.addEventListener("click", (Event) => {
+        current_color = Event.target.textContent;
+    });
 
     color_container.appendChild(color_element);
 }
@@ -18,7 +25,7 @@ let grid = document.querySelector(".grid");
 for (let i = 0; i < 16 * 16; i++) {
     let grid_cell = document.createElement("div");
     grid_cell.classList.add("grid-cell");
-    grid_cell.addEventListener("mouseover", (Event) => {
+    grid_cell.addEventListener("mousedown", (Event) => {
         console.log(Event.target);
         Event.target.style.background = current_color;
     });
